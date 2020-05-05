f23e1c31481638677be42e52a8fdde2bd2e571a5
Driptaroop Das <driptaroop.das@gmail.com>
2020-05-05 12:35:33 +0200

    added strikt (#17)

--- src/test/kotlin/org/dripto/githubissuetracker/GithubIssueTrackerApplicationTests.kt
+++ src/test/kotlin/org/dripto/githubissuetracker/GithubIssueTrackerApplicationTests.kt
@@ -2,6 +2,11 @@ package org.dripto.githubissuetracker
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import strikt.api.expectThat
+import strikt.assertions.isA
+import strikt.assertions.isEqualTo
+import strikt.assertions.isGreaterThan
+import strikt.assertions.isLessThan
 
 @SpringBootTest
 class GithubIssueTrackerApplicationTests {
@@ -10,4 +15,12 @@ class GithubIssueTrackerApplicationTests {
 	fun contextLoads() {
 	}
 
+	@Test
+	fun sampleTest() {
+		expectThat(1+1)
+				.isA<Int>()
+				.isGreaterThan(1)
+				.isLessThan(4)
+				.isEqualTo(2)
+	}
 }
