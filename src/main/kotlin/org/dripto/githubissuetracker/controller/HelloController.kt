9e24a7882410543c6cb8dd1f455e5426793ff2cd
Driptaroop Das <driptaroop.das@n26.com>
2020-05-05 21:59:55 +0200

    subtract

--- src/main/kotlin/org/dripto/githubissuetracker/controller/HelloController.kt
+++ src/main/kotlin/org/dripto/githubissuetracker/controller/HelloController.kt
@@ -1,7 +1,12 @@
 package org.dripto.githubissuetracker.controller
 
 import org.dripto.githubissuetracker.service.HelloService
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/hello")
