9e3ce6f4f68376c2779af1ceda608db4f3a4cada
Driptaroop Das <driptaroop.das@n26.com>
2020-05-06 12:52:24 +0200

    new actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,6 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.io.ByteArrayOutputStream
+import java.time.Instant.now
 
 plugins {
 	id("org.springframework.boot") version "2.2.6.RELEASE"
@@ -6,14 +8,29 @@ plugins {
 	id("org.jmailen.kotlinter") version "2.3.2"
 	id("org.sonarqube") version "2.8"
 	id("io.gitlab.arturbosch.detekt") version "1.8.0"
+	id("com.google.cloud.tools.jib") version "2.2.0"
+	id("org.owasp.dependencycheck") version "5.3.2.1"
 	kotlin("jvm") version "1.3.71"
 	kotlin("plugin.spring") version "1.3.71"
 	jacoco
 }
 
 group = "org.dripto"
-version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
+
+version = object {
+	private val version by lazy {
+		ByteArrayOutputStream().use {
+			exec {
+				commandLine = listOf("git", "rev-parse", "--short=6", "--verify", "HEAD")
+				standardOutput = it
+			}
+			it.toString().trim()
+		}
+	}
+
+	override fun toString() = version
+}
 
 repositories {
 	jcenter()
@@ -81,5 +98,21 @@ sonarqube {
 		property("sonar.login", System.getProperty("SONARCLOUD_TOKEN"))
 		property("sonar.kotlin.detekt.reportPaths", "$buildDir/reports/detekt/report.xml")
 		property("sonar.coverage.jacoco.xmlReportPaths", "$buildDir/reports/jacoco/test/jacocoTestReport.xml")
+	}
+}
+
+// Jib builds optimized Java docker containers
+jib {
+	container {
+		labels = mapOf("service" to rootProject.name)
+		ports = listOf("8080", "7091")
+		creationTime = now().toString()
+	}
+	from {
+		image = "amazoncorretto:11"
+	}
+	to {
+		image = "${rootProject.name}:${version.toString().take(6)}"
+		tags = setOf(version.toString(), "latest")
 	}
 }
