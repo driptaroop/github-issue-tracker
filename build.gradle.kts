4012b23840bc6475be91251305ff5204ec76b282
Driptaroop Das <driptaroop.das@gmail.com>
2020-05-09 18:25:08 +0200

    added heroku deploy

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,6 @@ plugins {
     id("org.jmailen.kotlinter") version "2.3.2"
     id("org.sonarqube") version "2.8"
     id("io.gitlab.arturbosch.detekt") version "1.8.0"
-    id("com.google.cloud.tools.jib") version "2.2.0"
     id("org.owasp.dependencycheck") version "5.3.2.1"
     kotlin("jvm") version "1.3.71"
     kotlin("plugin.spring") version "1.3.71"
@@ -98,21 +97,5 @@ sonarqube {
         property("sonar.login", System.getProperty("SONARCLOUD_TOKEN"))
         property("sonar.kotlin.detekt.reportPaths", "$buildDir/reports/detekt/report.xml")
         property("sonar.coverage.jacoco.xmlReportPaths", "$buildDir/reports/jacoco/test/jacocoTestReport.xml")
-    }
-}
-
-// Jib builds optimized Java docker containers
-jib {
-    container {
-        labels = mapOf("service" to rootProject.name)
-        ports = listOf("8080", "7091")
-        creationTime = now().toString()
-    }
-    from {
-        image = "amazoncorretto:11"
-    }
-    to {
-        image = "${rootProject.name}:${version.toString().take(6)}"
-        tags = setOf(version.toString(), "latest")
     }
 }
